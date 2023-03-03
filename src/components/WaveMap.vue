7fddb3965364913a693a6d5a4c849d01e5787b30
dxl <dxl@plotbridge.com>
2023-03-03 13:55:41 +0800

    show real time of wave, and support play now

--- src/components/WaveMap.vue
+++ src/components/WaveMap.vue
@@ -12,6 +12,11 @@ export default {
         // WaveSel
         ...mapStores(useWaveSelStore)
     },
+    data (){
+        return {
+            playing: false
+        }
+    },
     mounted() {
         // Initialize WaveSurfer instance
         this.waveform = WaveSurfer.create({
@@ -25,18 +30,40 @@ export default {
 
         // Load audio file
         this.waveform.load('/audio.mp3');
-        this.waveform.on('seek', (position) => {
-            this.waveSelStore.setPos(position)
-            // Do something with the cursor position, such as update a cursor position display
-        });
+        this.waveform.on('seek', this.updatePos)
+        this.waveform.on('audioprocess', this.updatePos)
+        this.waveform.on('play', ()=>{
+            this.playing = true
+        })
+        this.waveform.on('finish', ()=>{
+            this.playing = false
+        })
+
         this.waveform.on('region-created', (evt) => {
             console.log('Region created:', evt)
             // Do something with the region, such as store it in a data structure for later use
         });
     },
+    methods:{
+        updatePos(){
+            this.waveSelStore.setPos(this.waveform.getCurrentTime())
+        },
+        togglePlay(){
+            if(this.waveform.isPlaying()){
+                this.waveform.stop()
+            }else{
+                this.waveform.play()
+            }
+        }
+    }
 }
 </script>
 
 <template>
-    <div ref="waveform"></div>
+    <div>
+        <div ref="waveform"></div>
+        <div>
+            <button v-on:click="togglePlay"><span v-if='playing'>Stop</span><span v-else>Play</span></button>
+        </div>
+    </div>
 </template>
