44d171b3f57f46e8632f7b1117ace5827b0289f4
linqiu <1131868300@qq.com>
2023-04-07 18:01:54 +0800

    add disabledTime function

--- src/components/sublistModel/TimePicker.vue
+++ src/components/sublistModel/TimePicker.vue
@@ -12,24 +12,84 @@ export default {
     },
   },
   data() {
-    console.log("value = ", this.value);
+    // console.log("value = ", this.value);
     return {
       from: this.formTime(this.value.from * 1000),
       to: this.formTime(this.value.to * 1000),
       min: this.formTime(this.value.min * 1000),
       max: this.formTime(this.value.max * 1000),
-      disableHours: "",
-      disableMinutes: "",
-      disableSeconds: "",
+      fromlimit: {
+        hh: [],
+        mm: [],
+        ss: [],
+        // ms: [],
+      },
+      tolimit: {
+        hh: [],
+        mm: [],
+        ss: [],
+        // ms: [],
+      },
     };
   },
+  mounted() {
+    console.log("value in mounted", this.value);
+    if (this.value != "") {
+      this.getRange();
+    }
+  },
   methods: {
+    time_hms(time) {
+      const hh = time.getHours();
+      const mm = time.getMinutes();
+      const ss = time.getSeconds();
+      // const ms = time.getMilliseconds();
+      return [hh, mm, ss];
+    },
+    makeRange(start, end) {
+      if (start < end) {
+        let rangeArr = [];
+        for (let i = start; i <= end; i++) {
+          rangeArr.push(i);
+        }
+        return rangeArr;
+      } else {
+        console.log("start=", start, "end=", end);
+        return [0];
+      }
+    },
+    time_range(min, max, timeLimit) {
+      const idxArr = Object.keys(timeLimit);
+      for (let item in timeLimit) {
+        if (timeLimit.hasOwnProperty(item)) {
+          const idx = idxArr.indexOf(item);
+          if (idx != -1) {
+            timeLimit[item] = this.makeRange(min[idx], max[idx]);
+          }
+        }
+      }
+      return timeLimit;
+    },
     getRange() {
-      const minH = this.min.getHours();
-      const minM = this.min.getMinutes();
-      const minS = this.min.getSecondes();
+      const minR = this.time_hms(this.min);
+      const lFrom = this.time_hms(this.to);
+      const lTo = this.time_hms(this.from);
+      const maxR = this.time_hms(this.max);
+      this.time_range(minR, lFrom, this.fromlimit);
+      this.time_range(lTo, maxR, this.tolimit);
+      console.log("fromLimit--toLimit:", this.fromlimit, this.tolimit);
+    },
+    disableHH(limit_h) {
+      return limit_h;
+    },
+    disableMM(limit_m) {
+      return limit_m;
+    },
+    disableSS(limit_s) {
+      return limit_s;
     },
     incrTime(isFrom, sec) {
+      this.getRange();
       if (isFrom) {
         this.value.from += sec;
       } else {
@@ -90,6 +150,7 @@ export default {
               size="small"
               :default-value="to"
               format="H:mm:ss:SSS"
+              @change="setTime(false, this.to)"
             ></el-time-picker>
           </el-col>
           <el-col :span="4">
@@ -99,6 +160,7 @@ export default {
             <el-button class="comp-btn" @click="incrTime(false, -0.01)"
               >-
             </el-button>
+            
           </el-col>
         </el-row>
       </el-col>
